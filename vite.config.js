7f1ed3637bedac66717fb9381a21686bfc28d89c
Apolline-thy <apolline.thierry@etu.unilim.fr>
2025-10-25 21:31:57 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/Sae301/",
+  build: {
+    rollupOptions: {
+      input: {
+        accueil: resolve(__dirname, "index.html"),
+        paiment: resolve(__dirname, "pages/paiment.html"),
+        produit: resolve(__dirname, "pages/produit.html"),
+        panier: resolve(__dirname, "pages/panier.html"),
+      },
+    },
+  },
 });
