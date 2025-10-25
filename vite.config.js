49f12db43e3fe7a438f26e21d4727f7b54638d43
Apolline-thy <apolline.thierry@etu.unilim.fr>
2025-10-25 21:34:09 +0200

    pb3

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
   base: "/Sae301/",
   build: {
     rollupOptions: {
       input: {
         accueil: resolve(__dirname, "index.html"),
-        paiment: resolve(__dirname, "pages/paiment.html"),
-        produit: resolve(__dirname, "pages/produit.html"),
+        paiement: resolve(__dirname, "pages/paiement.html"),
         panier: resolve(__dirname, "pages/panier.html"),
+        produit: resolve(__dirname, "pages/produit.html"),
       },
     },
   },
