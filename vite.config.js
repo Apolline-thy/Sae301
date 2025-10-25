1f7de01340132074b541ff32640e168fee1f969b
Apolline-thy <apolline.thierry@etu.unilim.fr>
2025-10-25 20:50:49 +0200

    test6

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/Sae301",
+  base: "/Sae301/",
 });
